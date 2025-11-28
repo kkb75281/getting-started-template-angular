40e0a85c7a714c55b35728ce06555a9f76340f54
gyupp <kkb75281@gmail.com>
2025-11-28 16:06:13 +0900

    fix: update skapi-js dependency and remove OWNER_ID from Skapi initialization

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppComponent } from './app/app.component';
 import { Skapi } from 'skapi-js';
 
 const SERVICE_ID = '';
-const OWNER_ID = '';
 
-export const skapi = new Skapi(SERVICE_ID, OWNER_ID);
+export const skapi = new Skapi(SERVICE_ID);
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
