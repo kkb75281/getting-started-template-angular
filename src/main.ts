48c25da2dd8b0c15d0ab4005ee5276cc0fba0e9a
gyupp <kkb75281@gmail.com>
2026-08-11 17:13:17 +0900

    fix: update README and main.ts to replace SERVICE_ID with PROJECT_ID

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { Skapi } from 'skapi-js';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { AppComponent } from "./app/app.component";
+import { Skapi } from "skapi-js";
 
-const SERVICE_ID = '';
+const PROJECT_ID = "";
 
-export const skapi = new Skapi(SERVICE_ID);
+export const skapi = new Skapi(PROJECT_ID);
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
+    console.error(err),
 );
